ec22fc56cb587d2acd17045b80486ef3ec2b528a
Peter Jano <peter.jano88@gmail.com>
2019-01-03 13:08:03 +0100

    Half-ready answer function

--- server.py
+++ server.py
@@ -31,11 +31,13 @@ def add_a_question():
     return render_template("add_a_question.html", id=id, submission_time='1436520101', view_nr='5', vote_nr='5')
 
 
-@app.route('/question/<question_id>/new_answer' methods=['GET', 'POST'])
-def add_an_answer():
+@app.route('/question/<question_id>/new-answer', methods=['GET', 'POST'])
+def add_an_answer(question_id):
     if request.method == 'POST':
-        new_data = request.form.to_dict()
-        question_id = new_data['id']
+        pass
+    else:
+        answer_id = data_manager.create_id(data_manager.answer_csv)
+        return render_template('add_answer.html', question_id=question_id, answer_id=answer_id)
 
 
 
