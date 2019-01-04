c8232a7835801371ab6e9270249a98071193dac0
Peter Borsos <borsospeterlaszlo@gmail.com>
2019-01-04 10:56:00 +0100

    spring cleaning nice links and stuff...

--- server.py
+++ server.py
@@ -28,15 +28,15 @@ def display_question(question_id):
 @app.route("/add_a_question", methods=["GET", "POST"])
 def add_a_question():
     id = data_manager.create_id(data_manager.question_csv)
-    submission_time = 'default'
 
     if request.method == "POST":
         new_data = request.form.to_dict()
         question_id = new_data['id']
+        new_data['submission_time'] = time.time()
         data_manager.write_csv(data_manager.question_csv, data_manager.question_csv, data_manager.HEADER, new_data)
         return redirect(url_for('display_question', question_id=question_id))
 
-    return render_template("add_a_question.html", id=id, submission_time=submission_time, view_nr='5', vote_nr='5')
+    return render_template("add_a_question.html", id=id, submission_time='default', view_nr='5', vote_nr='5')
 
 
 @app.route('/question/<question_id>/new-answer', methods=['GET', 'POST'])
@@ -45,11 +45,12 @@ def add_an_answer(question_id):
 
     if request.method == 'POST':
         answer_data = request.form.to_dict()
+        answer_data['submission_time'] = time.time()
         data_manager.write_csv(data_manager.answer_csv, data_manager.answer_csv, data_manager.HEADER_ANSWER, answer_data)
         return redirect(url_for('display_question', question_id=question_id))
 
     return render_template('add_answer.html', question_id=question_id, answer_id=answer_id,
-                           submission_time='1436520101', vote_nr='5')
+                           submission_time='default', vote_nr='5')
 
 @app.route("/answer/<answer_id>/delete")
 def delete_answer(answer_id):
@@ -58,6 +59,20 @@ def delete_answer(answer_id):
         data_manager.delete_answer(answer_id)
         return redirect(url_for('display_question', question_id=question_id))
         #return ("POST")
+
+
+#@app.route("/question/<question_id>/vote", methods=['GET'])
+#def counting_votes(question_id):
+#    question_data = data_manager.find_question_from_id(question_id)
+#    vote_nr = question_data['vote_number']
+#    if vote_nr == None:
+#        vote_nr = 0
+#    else:
+#        vote_nr = int(question_data['vote_number']) + 1
+#    question_data['vote_number'] = vote_nr
+#    data_manager.write_csv(data_manager.question_csv, data_manager.question_csv, data_manager.HEADER, question_data)
+#    return redirect(url_for('display_question', question_id=question_id))
+
 
 if __name__ == '__main__':
     app.run(
